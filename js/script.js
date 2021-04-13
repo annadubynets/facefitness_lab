c7c01312ffa9740d54fab6fdc5b1d6ec90f9278f
Anna Dubynets <arogulina@gmail.com>
2021-04-13 21:20:18 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.testimonials .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
